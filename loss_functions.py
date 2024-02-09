909c8e65aee6fefe03c8423b9e3096d3254f92e4
Sai Sravan Yarlagadda <144404355+Sai-Yarlagadda@users.noreply.github.com>
2024-02-08 20:44:12 -0500

    Update loss_functions.py

--- loss_functions.py
+++ loss_functions.py
@@ -5,32 +5,25 @@ class loss_functions():
         self.y_preds = y_preds #softmax layer: list of probabilities
         self.y_target = y_target
 
-    def CrossEntropyLoss(self):
+    '''def CrossEntropyLoss(self):
         n_classes = len(self.y_preds)
         encoded_target = np.zeros_like(self.y_preds)
-        encoded_target[self.y_target -1] = 1
-        print(encoded_target)
+        encoded_target[self.y_target-1] = 1
+        print(f'target_in_loss: {encoded_target}')
 
         temp = 0
         for i in range(n_classes):
             print(temp)
             temp+=(encoded_target[i] * np.log(self.y_preds[i]))
-        return temp
+        return temp'''
+    
+    def cross_entropy_loss(self, epsilon=1e-15):
+        self.y_preds = np.clip(self.y_preds, epsilon, 1 - epsilon)
+        ce_loss = - (self.y_target * np.log(self.y_preds) + (1 - self.y_target) * np.log(1 - self.y_preds))
+        return np.sum(ce_loss)
 
     def l2loss(self):
         loss = np.sum(np.square(self.y_preds - self.y_target))
         return loss
 
 
-#cross checking class defined above
-y1 = np.array([0.1,0.2,0.8])
-y = 3
-loss = loss_functions(y_preds=y1, y_target=y)
-CE = loss.CrossEntropyLoss()
-print(CE)
-
-y1 = np.array([1,2,3])
-y2 =np.array([0,1,2])
-loss = loss_functions(y_preds=y1, y_target=y2)
-l2 = loss.l2loss()
-print(l2)
